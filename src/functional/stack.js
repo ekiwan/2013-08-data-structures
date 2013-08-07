7cf964951c75235c357fe7332aa3086a2968a4b9
Hack Reactor Students <github@hackreactor.com>
2013-08-07 15:28:53 -0700

    stack now pops in LIFO order. also refactor push

--- src/functional/stack.js
+++ src/functional/stack.js
@@ -8,11 +8,16 @@ var makeStack = function(){
   var stack = {};
 
   stack.push = function(value){
+    storage[size] = value;
     size++;
+    return value;
   };
 
   stack.pop = function(){
+    popped = storage[size-1];
+    delete storage[size];
     size && size--;
+    return popped;
   };
 
   stack.size = function(){
