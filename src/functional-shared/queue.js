3e766e9fd65aae3a09bdb426262ffd8acc20e047
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:17:02 -0700

    finished functional shared for stack and copied code into queue

--- src/functional-shared/queue.js
+++ src/functional-shared/queue.js
@@ -1,3 +1,31 @@
 var makeQueue = function(){
-	// Hey! Copy your code from src/functional/queue.js and paste it here
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var size = 0;// Hint: set an initial value here
+
+  // Implement the methods below
+  var queue = {};
+
+  queue.enqueue = function(value){
+    for (var i = size; i > 0; i--){
+      storage[i] = storage[i-1];
+    }
+    storage[0] = value;
+    size++;
+  };
+
+  queue.dequeue = function(){
+   var ind = String(size-1);
+   var dequeued = storage[ind];
+   delete storage[ind];
+   size && size--;
+   return dequeued;
+  };
+
+  queue.size = function(){
+    return size;
+  };
+
+  return queue;
 };
--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,34 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+// Use an object with numeric keys to store values
+// Implement the methods below
+  var stack = {};
+
+  stack.storage = {};
+
+  stack.stackSize=0;
+
+  _.extend(stack, makeStack.stackMethods);
+
+  return stack;
 };
+
+makeStack.stackMethods = {};
+
+makeStack.stackMethods.push = function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+    return value;
+  };
+
+makeStack.stackMethods.pop = function(){
+    var popped = this.storage[this.stackSize-1];
+    delete this.storage[this.stackSize];
+    this.stackSize && this.stackSize--;
+    return popped;
+  };
+
+makeStack.stackMethods.size = function(){
+    return this.stackSize;
+  };
+
+
