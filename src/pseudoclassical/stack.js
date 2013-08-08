5f6084a66b61106d500573d9fe55ed6b14b5d7c3
Hack Reactor Students <github@hackreactor.com>
2013-08-07 17:58:14 -0700

    finished pseudoclassical implementation of Stack

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,26 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+   //Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  this.storage = {};
+  this.stackSize=0;
 };
+
+Stack.stackMethods = {};
+
+Stack.prototype.push = function(value){
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+    return value;
+  };
+
+Stack.prototype.pop = function(){
+    var popped = this.storage[this.stackSize-1];
+    delete this.storage[this.stackSize];
+    this.stackSize && this.stackSize--;
+    return popped;
+  };
+
+Stack.prototype.size = function(){
+    return this.stackSize;
+  };
+
+
