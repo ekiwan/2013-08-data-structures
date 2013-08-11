a644246c49e66091755e84ef9e3b75ceae1625ef
ekiwan <elie.kiwan@gmail.com>
2013-08-11 13:35:54 -0700

    delete all my code and start over for practice

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -1,29 +1,19 @@
 var makeQueue = function(){
-  // Use an object with numeric keys to store values
+  
   var storage = {};
 
-  var size = 0;// Hint: set an initial value here
+  var size = 0;
 
-  // Implement the methods below
+  
   var queue = {};
 
-  queue.enqueue = function(value){
-    for (var i = size; i > 0; i--){
-      storage[i] = storage[i-1];
-    }
-    size++;
-    storage[0] = value;
+  queue.enqueue = function(){
   };
 
   queue.dequeue = function(){
-   var front = storage[size-1];
-   delete storage[size-1];
-   size && size--;
-   return front;
   };
 
   queue.size = function(){
-    return size;
   };
 
   return queue;
