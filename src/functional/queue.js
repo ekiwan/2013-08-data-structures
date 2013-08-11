bddf1875fb1ad34406659292b66679bc12e493a1
ekiwan <elie.kiwan@gmail.com>
2013-08-11 13:57:02 -0700

    rewrite queue

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -1,19 +1,29 @@
 var makeQueue = function(){
-  
+  // Use an object with numeric keys to store values
   var storage = {};
 
-  var size = 0;
+  var size = 0;// Hint: set an initial value here
 
-  
+  // Implement the methods below
   var queue = {};
 
-  queue.enqueue = function(){
+  queue.enqueue = function(value){
+    for (var i = size; i > 0; i--) {
+      storage[i] = storage[i - 1];
+    }
+    storage[0] = value;
+    size++;
   };
 
   queue.dequeue = function(){
+    var front = storage[size - 1];
+    delete front;
+    size && size--;
+    return front;
   };
 
   queue.size = function(){
+    return size;
   };
 
   return queue;
