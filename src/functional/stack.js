417de12c1ebbca0ddc28c12945a0768ce652efdc
ekiwan <elie.kiwan@gmail.com>
2013-08-11 15:00:49 -0700

    rewrite stack. cleaner, clearer code with fewer lines

--- src/functional/stack.js
+++ src/functional/stack.js
@@ -8,9 +8,15 @@ var makeStack = function(){
   var stack = {};
 
   stack.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   stack.pop = function(){
+  var popped = storage[size - 1];
+  delete storage[size - 1];
+  size && size--;
+  return popped;
   };
 
   stack.size = function(){
