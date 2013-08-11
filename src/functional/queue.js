d2552a8b124ab52ff66c544b6578ab38a9a6a7e2
ekiwan <elie.kiwan@gmail.com>
2013-08-11 14:02:58 -0700

    rewrite queue again

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -8,20 +8,23 @@ var makeQueue = function(){
   var queue = {};
 
   queue.enqueue = function(value){
+    for (var i = size; i > 0; i--) {
+      storage[i] = storage[i - 1];
+    }
+    storage[0] = value;
+    size ++;            
   };
 
   queue.dequeue = function(){
+    var front = storage[size - 1];
+    delete storage[size -1];
+    size && size--;
+    return front;
   };
 
   queue.size = function(){
+    return size;
   };
 
   return queue;
 };
-
-
-/*
-0:c
-1:b
-2:a
-*/
