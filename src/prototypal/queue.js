f74558dcb6290f16ca8141608ffdacf8d6093fe1
ekiwan <elie.kiwan@gmail.com>
2013-08-11 19:41:20 -0700

    rewrote queue with prototypal instantiation

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,34 +1,29 @@
-var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
-  var queue = Object.create(Queue.queueMethods);
+var Queue = function(){
+  var queue = Object.create(queueMethods);
   queue.storage = {};
   queue.queueSize = 0;
-
-  // Hint: set an initial value here
-  // Implement the methods below
   return queue;
 };
 
+queueMethods = {};
+  
 
-Queue.queueMethods = {};
+queueMethods.enqueue = function(value){
+  for (var i = this.queueSize; i > 0; i--) {
+    this.storage[i] = this.storage[i - 1];
+  }
+  this.storage[0] = value;
+  this.queueSize ++;            
+};
 
-  Queue.queueMethods.enqueue = function(value){
-    for (var i = this.queueSize; i > 0; i--){
-      this.storage[i] = this.storage[i-1];
-    }
-    this.storage[0] = value;
-    this.queueSize++;
-  };
+queueMethods.dequeue = function(){
+  var front = this.storage[this.queueSize - 1];
+  delete this.storage[this.queueSize -1];
+  this.queueSize && this.queueSize--;
+  return front;
+};
 
-  Queue.queueMethods.dequeue = function(){
-   var ind = String(this.queueSize-1);
-   var dequeued = this.storage[ind];
-   delete this.storage[ind];
-   this.queueSize && this.queueSize--;
-   return dequeued;
-  };
-
-  Queue.queueMethods.size = function(){
-    return this.queueSize;
-  };
+queueMethods.size = function(){
+  return this.queueSize;
+};
 
