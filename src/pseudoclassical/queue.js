8569b48d5b61f951978b4b3f29ee64c42f855cf4
ekiwan <elie.kiwan@gmail.com>
2013-08-11 19:59:03 -0700

    rewrite queue in pseudoclassical style

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,29 +1,27 @@
 var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
   this.storage = {};
   this.queueSize = 0;
-
-  // Hint: set an initial value here
-  // Implement the methods below
 };
 
-  Queue.prototype.enqueue = function(value){
-    for (var i = this.queueSize; i > 0; i--){
-      this.storage[i] = this.storage[i-1];
-    }
-    this.storage[0] = value;
-    this.queueSize++;
-  };
+Queue.prototype.enqueue = function(value){
+  for (var i = this.queueSize; i > 0; i--) {
+    this.storage[i] = this.storage[i - 1];
+  }
+  this.storage[0] = value;
+  this.queueSize ++;            
+};
 
-  Queue.prototype.dequeue = function(){
-   var ind = String(this.queueSize-1);
-   var dequeued = this.storage[ind];
-   delete this.storage[ind];
-   this.queueSize && this.queueSize--;
-   return dequeued;
-  };
+Queue.prototype.dequeue = function(){
+  var front = this.storage[this.queueSize - 1];
+  delete this.storage[this.queueSize -1];
+  this.queueSize && this.queueSize--;
+  return front;
+};
 
-  Queue.prototype.size = function(){
-    return this.queueSize;
-  };
+Queue.prototype.size = function(){
+  return this.queueSize;
+};
+
+
+
 
