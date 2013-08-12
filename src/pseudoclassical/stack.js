88b9711cf503c39a39f0a6e86e7f872082023a8f
ekiwan <elie.kiwan@gmail.com>
2013-08-11 20:10:21 -0700

    rewrite stack in pseudoclassical style

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,24 +1,23 @@
 var Stack = function() {
-   //Hey! Copy your code from src/functional-shared/stack.js and paste it here
   this.storage = {};
-  this.stackSize=0;
+  this.stackSize=0; 
 };
+  
 
 Stack.prototype.push = function(value){
-    this.storage[this.stackSize] = value;
-    this.stackSize++;
-    return value;
-  };
+  this.storage[this.stackSize] = value;
+  this.stackSize++;
+};
 
 Stack.prototype.pop = function(){
-    var popped = this.storage[this.stackSize-1];
-    delete this.storage[this.stackSize];
-    this.stackSize && this.stackSize--;
-    return popped;
-  };
+var popped = this.storage[this.stackSize - 1];
+delete this.storage[this.stackSize - 1];
+this.stackSize && this.stackSize--;
+return popped;
+};
 
 Stack.prototype.size = function(){
-    return this.stackSize;
-  };
+  return size;
+};
 
 
